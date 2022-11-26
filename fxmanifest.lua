56b54e7c742a5987290e3677bb64bb957bf975e9
Kiminaze <kiminaze@yahoo.de>
2022-11-26 12:25:29 +0100

    Reformat fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,46 +1,46 @@
-fx_version 'cerulean'
-games { 'gta5' }
+fx_version "cerulean"
+games { "gta5" }
 
-author 'Philipp Decker'
-description 'Lets you create context menus.'
-version '3.0.0_dev'
+author "Philipp Decker"
+description "Lets you create context menus."
+version "3.0.0_dev"
 
 client_scripts {
-	'Utils/screenToWorld.lua',
-	'Utils/TextAlignment.lua',
-	'Utils/TextFont.lua',
-	'Utils/Log.lua',
-	'Utils/Color.lua',
+	"Utils/screenToWorld.lua",
+	"Utils/TextAlignment.lua",
+	"Utils/TextFont.lua",
+	"Utils/Log.lua",
+	"Utils/Color.lua",
 
-	'DefaultValues.lua',
+	"DefaultValues.lua",
 
-	'Graphics2D/Object2D.lua',
-	'Graphics2D/Rect.lua',
-	'Graphics2D/Text.lua',
-	'Graphics2D/Sprite.lua',
-	'Graphics2D/SpriteUV.lua',
-	'Graphics2D/Container.lua',
-	'Graphics2D/Border.lua',
+	"Graphics2D/Object2D.lua",
+	"Graphics2D/Rect.lua",
+	"Graphics2D/Text.lua",
+	"Graphics2D/Sprite.lua",
+	"Graphics2D/SpriteUV.lua",
+	"Graphics2D/Container.lua",
+	"Graphics2D/Border.lua",
 
-	'Items/BaseItem.lua',
-	'Items/SeparatorItem.lua',
-	'Items/ScrollItem.lua',
-	'Items/PageItem.lua',
-	'Items/TextItem.lua',
-	'Items/Item.lua',
-	'Items/SpriteItem.lua',
-	'Items/SubmenuItem.lua',
-	'Items/CheckboxItem.lua',
+	"Items/BaseItem.lua",
+	"Items/SeparatorItem.lua",
+	"Items/ScrollItem.lua",
+	"Items/PageItem.lua",
+	"Items/TextItem.lua",
+	"Items/Item.lua",
+	"Items/SpriteItem.lua",
+	"Items/SubmenuItem.lua",
+	"Items/CheckboxItem.lua",
 
-	'Menu/Menu.lua',
-	'Menu/ScrollMenu.lua',
-	'Menu/PageMenu.lua',
-	'Menu/MenuPool.lua',
+	"Menu/Menu.lua",
+	"Menu/ScrollMenu.lua",
+	"Menu/PageMenu.lua",
+	"Menu/MenuPool.lua",
 
-	'Menu/ExportMenu.lua'
+	"Menu/ExportMenu.lua"
 }
 
 -- example
---client_scripts {
---	'example/*.lua'
---}
+client_scripts {
+	"example/*.lua"
+}
